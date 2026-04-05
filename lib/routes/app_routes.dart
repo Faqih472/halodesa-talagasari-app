97403231303838254e9ad74efffa282cf11a24f5
Faqih472 <mfaqih47289@gmail.com>
2026-04-05 14:32:14 +0700

    feat(routes): menambahkan konstanta route aplikasi
    - Menambahkan route LOGIN
    - Menambahkan route REGISTER
    - Menambahkan route DASHBOARD_WARGA
    - Menambahkan route DASHBOARD_ADMIN
    - Menambahkan route DASHBOARD_KADES
    - Digunakan untuk navigasi GetX

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+abstract class Routes {
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const DASHBOARD_WARGA = '/dashboard-warga';
+  static const DASHBOARD_ADMIN = '/dashboard-admin';
+  static const DASHBOARD_KADES = '/dashboard-kades';
+}
