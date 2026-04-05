92cf5e22c7d25ac0993a03569d505df856081489
IzzudinAl <136605534+Faqih472@users.noreply.github.com>
2026-04-06 04:28:20 +0700

    Delete lib directory

--- lib/routes/app_routes.dart
+++
@@ -1,7 +0,0 @@
-abstract class Routes {
-  static const LOGIN = '/login';
-  static const REGISTER = '/register';
-  static const DASHBOARD_WARGA = '/dashboard-warga';
-  static const DASHBOARD_ADMIN = '/dashboard-admin';
-  static const DASHBOARD_KADES = '/dashboard-kades';
-}
